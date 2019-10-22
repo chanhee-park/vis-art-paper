af64fd86f26e0ed96393831da4514192d70da842
chanhee13p <chanhee13p@gmail.com>
2019-10-22 21:57:51 +0900

    레전드 수정

--- scripts/legend.js
+++ scripts/legend.js
@@ -1,186 +1,188 @@
 const svg = d3.select(`#legend`);
 
-/** 컬러 인코딩  **/
+/****************** 노드 컬러 ******************/
 svg.append('text')
   .text('Color of Keyword Nodes')
   .attrs({
-    x: 350,
+    x: 100,
     y: 150,
-    'text-anchor': 'middle',
+    'text-anchor': 'start',
     'alignment-baseline': 'central',
-    'font-size': '21px',
-    'font-family': "Noto Sans KR",
-    'font-weight': 300,
-  });
-svg.append('text')
-  .text('by Parts of Speech')
-  .attrs({
-    x: 350,
-    y: 180,
-    'text-anchor': 'middle',
-    'alignment-baseline': 'central',
-    'font-size': '21px',
+    'font-size': '18px',
     'font-family': "Noto Sans KR",
     'font-weight': 300,
   });
 
-svg.append('rect')
+svg.append('text')
+  .text('by Parts of Speech')
   .attrs({
-    x: 250,
-    y: 60,
-    width: 25,
-    height: 25,
+    x: 100,
+    y: 180,
+    'text-anchor': 'start',
+    'alignment-baseline': 'central',
+    'font-size': '18px',
+    'font-family': "Noto Sans KR",
+    'font-weight': 300,
+  });
+
+// 색상 범례 원 - 명사
+svg.append('circle')
+  .attrs({
+    cx: 112,
+    cy: 80,
+    r: 10,
     fill: colormap.n,
   });
 
 svg.append('text')
   .text('Noun')
   .attrs({
-    x: 280,
-    y: 60,
+    x: 130,
+    y: 80,
     'text-anchor': 'start',
-    'alignment-baseline': 'central',
-    'font-size': '18px',
+    'alignment-baseline': 'middle',
+    'font-size': '16px',
     'font-family': "Noto Sans KR",
-    'font-weight': 300,
+    'font-weight': 100,
   });
 
-svg.append('rect')
+// 색상 범례 원 - 동사
+svg.append('circle')
   .attrs({
-    x: 250,
-    y: 100,
-    width: 25,
-    height: 25,
+    cx: 112,
+    cy: 115,
+    r: 10,
     fill: colormap.v,
   });
 
 svg.append('text')
   .text('Verb')
   .attrs({
-    x: 280,
-    y: 100,
+    x: 130,
+    y: 115,
     'text-anchor': 'start',
-    'alignment-baseline': 'central',
-    'font-size': '18px',
+    'alignment-baseline': 'middle',
+    'font-size': '16px',
     'font-family': "Noto Sans KR",
-    'font-weight': 300,
+    'font-weight': 100,
   });
 
-svg.append('rect')
+// 색상 범례 원 - 형용사
+svg.append('circle')
   .attrs({
-    x: 380,
-    y: 60,
-    width: 25,
-    height: 25,
+    cx: 215,
+    cy: 80,
+    r: 10,
     fill: colormap.a,
   });
 
 svg.append('text')
-  .text('Verb')
+  .text('Adjective')
   .attrs({
-    x: 410,
-    y: 60,
+    x: 233,
+    y: 80,
     'text-anchor': 'start',
-    'alignment-baseline': 'central',
-    'font-size': '18px',
+    'alignment-baseline': 'middle',
+    'font-size': '16px',
     'font-family': "Noto Sans KR",
-    'font-weight': 300,
+    'font-weight': 100,
   });
 
-svg.append('rect')
+// 색상 범례 원 - 기타
+svg.append('circle')
   .attrs({
-    x: 380,
-    y: 100,
-    width: 25,
-    height: 25,
+    cx: 215,
+    cy: 115,
+    r: 10,
     fill: colormap.x,
   });
-
 svg.append('text')
-  .text('Verb')
+  .text('Ect.')
   .attrs({
-    x: 410,
-    y: 100,
+    x: 233,
+    y: 115,
     'text-anchor': 'start',
-    'alignment-baseline': 'central',
-    'font-size': '18px',
+    'alignment-baseline': 'middle',
+    'font-size': '16px',
     'font-family': "Noto Sans KR",
-    'font-weight': 300,
+    'font-weight': 100,
   });
+/****************** 노드 컬러 ******************/
 
-/** 노드 사이즈  **/
+/****************** 노드 사이즈 ******************/
 svg.append('text')
   .text('Size of Keyword Nodes')
   .attrs({
-    x: 650,
+    x: 370,
     y: 150,
-    'text-anchor': 'middle',
+    'text-anchor': 'start',
     'alignment-baseline': 'central',
-    'font-size': '21px',
+    'font-size': '18px',
     'font-family': "Noto Sans KR",
     'font-weight': 300,
   });
 svg.append('text')
   .text('by Frequencies')
   .attrs({
-    x: 650,
+    x: 370,
     y: 180,
-    'text-anchor': 'middle',
+    'text-anchor': 'start',
     'alignment-baseline': 'central',
-    'font-size': '21px',
+    'font-size': '18px',
     'font-family': "Noto Sans KR",
     'font-weight': 300,
   });
 
-for (let cnt = 5; cnt <= 20; cnt += 5) {
+for (let cnt = 5; cnt <= 15; cnt += 5) {
   const r = Math.sqrt(cnt) * 8;
   svg.append('circle')
     .attrs({
-      cx: 650,
-      cy: 120 - r,
+      cx: 445,
+      cy: 130 - r,
       r: r,
       fill: 'none',
       stroke: '#555'
     });
   svg.append('line')
     .attrs({
-      x1: 650,
-      x2: 700,
-      y1: 120 - 2 * r,
-      y2: 120 - 2 * r,
+      x1: 445,
+      x2: 480,
+      y1: 130 - 2 * r,
+      y2: 130 - 2 * r,
       stroke: '#aaa'
     })
   svg.append('text').text(cnt).attrs({
-    x: 700,
-    y: 120 - 2 * r - 5,
+    x: 505,
+    y: 130 - 2 * r,
     'text-anchor': 'start',
     'alignment-baseline': 'middle',
     'font-size': '12px',
     'font-family': "Noto Sans KR",
-    'font-weight': 300,
+    'font-weight': 100,
   })
 }
+/****************** 노드 사이즈 ******************/
 
-/** 엣지 사이즈  **/
+/****************** 엣지 두께 ******************/
 svg.append('text')
   .text('Thickness of Arcs')
   .attrs({
-    x: 950,
+    x: 640,
     y: 150,
-    'text-anchor': 'middle',
+    'text-anchor': 'start',
     'alignment-baseline': 'central',
-    'font-size': '21px',
+    'font-size': '18px',
     'font-family': "Noto Sans KR",
     'font-weight': 300,
   });
 svg.append('text')
   .text('by Co-occurrences')
   .attrs({
-    x: 950,
+    x: 640,
     y: 180,
-    'text-anchor': 'middle',
+    'text-anchor': 'start',
     'alignment-baseline': 'central',
-    'font-size': '21px',
+    'font-size': '18px',
     'font-family': "Noto Sans KR",
     'font-weight': 300,
   });
@@ -189,8 +191,8 @@ svg.append('text')
 for (let thickness = 0; thickness <= 30; thickness += 0.1) {
   svg.append('rect')
     .attrs({
-      x: 870 + thickness * 5,
-      y: 100 - thickness / 2,
+      x: 640 + thickness * 5,
+      y: 113 - thickness / 2,
       width: 0.3,
       height: thickness,
       fill: '#000',
@@ -198,20 +200,21 @@ for (let thickness = 0; thickness <= 30; thickness += 0.1) {
 }
 
 svg.append('text').text('min').attrs({
-  x: 870,
-  y: 60,
-  'text-anchor': 'middle',
+  x: 640,
+  y: 70,
+  'text-anchor': 'start',
   'alignment-baseline': 'middle',
   'font-size': '12px',
   'font-family': "Noto Sans KR",
-  'font-weight': 300,
+  'font-weight': 100,
 });
 svg.append('text').text('max').attrs({
-  x: 1020,
-  y: 60,
-  'text-anchor': 'middle',
+  x: 790,
+  y: 70,
+  'text-anchor': 'end',
   'alignment-baseline': 'middle',
   'font-size': '12px',
   'font-family': "Noto Sans KR",
-  'font-weight': 300,
+  'font-weight': 100,
 });
+/****************** 엣지 두께 ******************/
